404419fd729520b1f50a4861f4ba19c01761dd97
Henrique Dezani <professorhenriquedezani@gmail.com>
2024-09-10 18:14:43 -0300

    MVC (Model)

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using BooksWeb.Models;
 
 namespace BooksWeb.Controllers;
 
@@ -10,6 +11,18 @@ public class BookController : Controller
     // action = método
     public ActionResult Read()
     {
-        return View();
+        Book book1 = new Book();
+        book1.Title = "Código da Vinci";
+        book1.Author = "Dan Brown";
+
+        Book book2 = new Book();
+        book2.Title = "Símbolo Perdido";
+        book2.Author = "Dan Brown";
+
+        List<Book> books = new List<Book>();
+        books.Add(book1);
+        books.Add(book2);
+        
+        return View(books);
     }
 }
---
+++ Models/Book.cs
@@ -0,0 +1,23 @@
+namespace BooksWeb.Models;
+
+public class Book
+{
+    // private string title;
+
+    // public string GetTitle() {
+    //     return title;
+    // }
+
+    // public void SetTitle(string title) {
+    //     this.title = title;
+    // }
+
+    // PROPRIEDADE:
+    // public string Title {
+    //     get { return title; }
+    //     set { title = value; }
+    // }
+    // PROPRIEDADE AUTOMÁTICA:
+    public string Title {get; set; }
+    public string Author { get; set; }
+}
