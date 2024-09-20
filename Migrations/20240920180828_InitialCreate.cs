dc853ca6d63946d17e055bf463198d1ce8b7059c
Henrique Dezani <professorhenriquedezani@gmail.com>
2024-09-20 15:13:13 -0300

    add sqlite

---
+++ Migrations/20240920180828_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    Synopsis = table.Column<string>(type: "TEXT", nullable: false),
+                    Gender = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // middleware
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<BookDatabase>(options => options.UseInMemoryDatabase("db"));
+// builder.Services.AddDbContext<BookDatabase>(options => options.UseInMemoryDatabase("db"));
+builder.Services.AddDbContext<BookDatabase>(options => options.UseSqlite("Data Source=books.db"));
 
 
 var app = builder.Build();
